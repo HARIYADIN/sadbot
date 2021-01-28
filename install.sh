aaf8041a039d6f7c9ee96c48f5f1e963927891b8
Ramlan666 <77728778+Ramlan666@users.noreply.github.com>
2021-01-28 12:33:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ npm i got
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[*] All dependencies have been installed, please run the command \"node index.js\" to immediately start the script"
